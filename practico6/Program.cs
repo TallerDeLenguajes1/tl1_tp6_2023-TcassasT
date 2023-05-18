cb23a8ab512c5be5a6b434c098ea9ad6fef40f89
Tomás Cassas <tcassas@archt.com.ar>
2023-05-18 19:24:46 -0300

    Ejercicio 3 CalculadoraV2 completa

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,23 +1,42 @@
-﻿Random random = new Random();
+﻿int numeroParaAplicarPropiedades;
+Random random = new Random();
 
-Console.Write("Ingrese un numero A: ");
-int inputParsesado;
+Console.Write("Ingrese un numero para aplicar propiedades: ");
+if (int.TryParse(Console.ReadLine(), out numeroParaAplicarPropiedades)) {
+  Console.WriteLine(" |" + numeroParaAplicarPropiedades + "| = " + Math.Abs(numeroParaAplicarPropiedades));
+  Console.WriteLine(" " + numeroParaAplicarPropiedades + "² = " + Math.Pow(numeroParaAplicarPropiedades, 2));
+  Console.WriteLine(" ²√" + numeroParaAplicarPropiedades + " = " + Math.Sqrt(numeroParaAplicarPropiedades));
+  Console.WriteLine(" SEN(" + numeroParaAplicarPropiedades + ") = " + Math.Sin(numeroParaAplicarPropiedades));
+  Console.WriteLine(" COS(" + numeroParaAplicarPropiedades + ") = " + Math.Cos(numeroParaAplicarPropiedades));
 
-if (int.TryParse(Console.ReadLine(), out inputParsesado)) {
-  int numeroAleatorio = random.Next(100) + 1;
+  float numeroDecimal = ((float) random.NextDouble() * (float) (10));
+  Console.WriteLine(" Parte entera de " + numeroDecimal + " = " + (Math.Truncate(numeroDecimal)));
+} else {
+  Console.WriteLine(" El numero ingresado no es valido.");
+}
 
-  Console.WriteLine("Basado en el numero aleatorio B: " + numeroAleatorio);
+bool numerosACompararValidos = true;
+int numeroACompararA, numeroACompararB;
 
-  if (inputParsesado > 0) {
-    int auxiliar = inputParsesado;
+Console.WriteLine("");
+Console.WriteLine("Ingrese un numero A para comparar: ");
+if (!int.TryParse(Console.ReadLine(), out numeroACompararA)) {
+  numerosACompararValidos = false;
+}
 
-    inputParsesado = numeroAleatorio;
-    numeroAleatorio = auxiliar;
+Console.WriteLine("Ingrese un numero B para comparar: ");
+if (!int.TryParse(Console.ReadLine(), out numeroACompararB)) {
+  numerosACompararValidos = false;
+}
 
-    Console.WriteLine("Resultado invertido A: " + inputParsesado + " B: " + numeroAleatorio);
+if (numerosACompararValidos) {
+  Console.WriteLine("Dados los numeros ingresados, se sabe que:");
+  if (numeroACompararA == numeroACompararB) {
+    Console.WriteLine(" Ambos numeros son iguales: " + numeroACompararA + " = " + numeroACompararB);
   } else {
-    Console.WriteLine("Resultado A: " + inputParsesado + " B: " + numeroAleatorio);
+    Console.WriteLine(" Maximo: " + (numeroACompararA > numeroACompararB ? numeroACompararA : numeroACompararB));
+    Console.WriteLine(" Minimo: " + (numeroACompararA < numeroACompararB ? numeroACompararA : numeroACompararB));
   }
 } else {
-  Console.WriteLine("El valor ingresado o es nulo o no es un numero.");
+  Console.WriteLine("Numeros a comparar invalidos, por favor ingrese numeros.");
 }
